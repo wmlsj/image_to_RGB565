d1624ef61f91768e099f618d76f1384e7b0ba327
wmlsj666 <52365743+wmlsj@users.noreply.github.com>
2023-05-29 16:25:13 +0800

    Update main.py
    将图片长宽写入结构体，方便push图片时直接通过 图片名.width和height直接调用图片尺寸

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from PIL import Image
+from PIL import Image, ImageSequence
 
 
 def generate_img_h(path):
@@ -8,6 +8,11 @@ def generate_img_h(path):
     files = os.listdir(path)
     f = open('img.h', 'w')
     f.write('#include <pgmspace.h>\n')
+    f.write('typedef struct image{\n')
+    f.write('    int width;\n')
+    f.write('    int height;\n')
+    f.write('    uint16_t pixels[];\n')
+    f.write('}image;\n\n')
     for file in files:
         if file.endswith('.jpg') or file.endswith('.png'):
             file_count += 1
@@ -16,22 +21,24 @@ def generate_img_h(path):
             pixels = img.load()
             name = file.split('.')[0]
             print(f'{name}: {width}x{height} pixels')
-            total_size += width * height * 2  # 每个像素2字节
-            f.write(f'const uint16_t {name}[{width * height}] PROGMEM ={{\n')
+            total_size += width * height * 2
+            f.write(f'const image {name} = {{\n')
+            f.write(f'    {width},\n')
+            f.write(f'    {height},\n')
+            f.write(f'    {{\n\t\t')
             for y in range(height):
                 for x in range(width):
                     if x % 16 == 0 and x != 0:
-                        f.write('\n')
+                        f.write('\n\t\t')
                     r, g, b = pixels[x, y]
                     color = (b & 0xf8) << 8 | (g & 0xfc) << 3 | r >> 3
-                    f.write(f'0x{color:06x}, ')  # 这里修改为%06x
-                f.write('\n')
-            f.write('};\n\n')
-            total_size /= 1024  # 转换为KB
+                    f.write(f'0x{color:06x}, ')
+                f.write('\n\t\t')
+            f.write('}};\n\n\n')
+            total_size /= 1024
     f.close()
     print(f'Total size: {total_size} KB')
     print(f'File count: {file_count}')
-
 
 if __name__ == '__main__':
     generate_img_h("image")
